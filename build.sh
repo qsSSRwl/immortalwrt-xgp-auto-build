d98efc10c8dad01a0a2f4a720f2bf60cbc73a154
qsyrw <43160479+qsyrw@users.noreply.github.com>
2025-11-23 21:09:46 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -89,4 +89,4 @@ fi
 echo "make download"
 make download -j8 || { echo "download failed"; exit 1; }
 echo "make lede"
-make V=0 -j$(nproc) || { echo "make failed"; exit 1; }
+make -j1 V=s || { echo "make failed"; exit 1; }
